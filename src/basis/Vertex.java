6152adf01a652270bb75e3181f63bec5d40a30d1
bluestyle97 <690147660@qq.com>
2017-09-16 13:32:55 +0800

    Directed graph's basic definition

---
+++ src/basis/Vertex.java
@@ -0,0 +1,39 @@
+package basis;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Vertex {
+	public String name;
+	public HashSet<Vertex> predecessors;
+	public HashSet<Vertex> successors;
+	public HashMap<Vertex, Integer> weights;
+	
+	public Vertex() {
+		this.name = null;
+		this.predecessors = new HashSet<>();
+		this.successors = new HashSet<>();
+		this.weights = new HashMap<>();
+	}
+	
+	public Vertex(String name) {
+		this.name = name;
+		this.predecessors = new HashSet<>();
+		this.successors = new HashSet<>();
+		this.weights = new HashMap<>();
+	}
+	
+	public boolean equals(Vertex v) {
+		return this.name.equals(v.name);
+	}
+	
+	@Override
+	public String toString() {
+		return this.name;
+	}
+	
+	@Override
+	public int hashCode() {
+		return this.name.hashCode();
+	}
+}
